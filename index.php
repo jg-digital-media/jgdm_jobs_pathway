4e7a02d99cbd8cbbf6451920bf04b0aefbb294a4
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-12 09:36:28 +0100

    add homepage file with title saved to var

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+    $website_title = "Jobs Pathway";
+    $website_description = "Keep track of your way to your professional life";
+    $website_keywords = "Jobs Pathway, Jobs, Pathway, Jobs Tracker, Job Tracker, Job Tracking, Job Tracking System";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $website_title ?></title>
+</head>
+<body>
+
+    <h1> <?php echo $website_title ?> </h1>
+
+    <h2>Keep track of your way to your professional life</h2>
+    
+</body>
+</html>
