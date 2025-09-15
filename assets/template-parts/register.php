fdf704e5cf7772f017516c4cdc4b48df5ffb2e54
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-15 10:06:11 +0100

    develop registration form

--- assets/template-parts/register.php
+++ assets/template-parts/register.php
@@ -2,5 +2,19 @@
  <form name="main_form_register" action="list.php" method="post" id="" class="form form---register">
    
     <p>Registration Form</p>
+    
+    <label for="username">Username: </label>
+    <input type="text" name="username" class="login---input" id="username" placeholder="Enter your name">
+
+    <br>
+    <label for="email">Email: </label>
+    <input type="email" name="email" class="login---input" id="email" placeholder="Enter your email">
+
+    <br>
+    <label for="message">Password: </label>
+    <textarea name="message" id="message" class="login---input" cols="30" rows="10" placeholder="Enter your password"></textarea>
+
+    <br>
+    <input type="submit" value="Submit" class="button button---login">
 
 </form>
--- index.php
+++ index.php
@@ -80,23 +80,7 @@
 
     <section class="container---element--login">
 
-        <form name="main_login" action="list.php" method="post" id="" class="">
-
-            <label for="username">Username: </label>
-            <input type="text" name="username" class="login---input" id="username" placeholder="Enter your name">
-
-            <br>
-            <label for="email">Email: </label>
-            <input type="email" name="email" class="login---input" id="email" placeholder="Enter your email">
-
-            <br>
-            <label for="message">Password: </label>
-            <textarea name="message" id="message" class="login---input" cols="30" rows="10" placeholder="Enter your password"></textarea>
-
-            <br>
-            <input type="submit" value="Submit" class="button button---login">
-
-        </form>
+        <?php require "assets/template-parts/login.php"; ?>
 
     </section>
 
