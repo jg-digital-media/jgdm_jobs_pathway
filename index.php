d95eed6ceb42d90feda10f6ada0c07d430de9347
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-15 10:43:58 +0100

    button login class for login screen

--- index.php
+++ index.php
@@ -32,65 +32,65 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Meta Tags -->
-    <meta name="description" content="<?php echo $page_description; ?>">
-    <meta name="keywords" content="<?php echo $page_keywords; ?>"> 
-    <meta name="image" content="<?php echo $page_image; ?>">
+        <!-- Meta Tags -->
+        <meta name="description" content="<?php echo $page_description; ?>">
+        <meta name="keywords" content="<?php echo $page_keywords; ?>"> 
+        <meta name="image" content="<?php echo $page_image; ?>">
 
-    <!-- FACEBOOK: Open Graph -->
-    <meta property="og:title" content="<?php echo $page_title; ?> ">
-    <meta property="og:description" content="<?php echo $page_description; ?>">
-    <meta property="og:image" content="<?php echo $page_image; ?>">
-    <meta property="og:url" content="<?php echo $page_url; ?>">
+        <!-- FACEBOOK: Open Graph -->
+        <meta property="og:title" content="<?php echo $page_title; ?> ">
+        <meta property="og:description" content="<?php echo $page_description; ?>">
+        <meta property="og:image" content="<?php echo $page_image; ?>">
+        <meta property="og:url" content="<?php echo $page_url; ?>">
 
-    <!-- TWITTER: Open Graph -->
-    <meta name="twitter:title" content="<?php echo $page_title; ?> ">
-    <meta name="twitter:description" content="<?php echo $page_description; ?>">
-    <meta name="twitter:image" content="<?php echo $page_image; ?>">
-    <meta name="twitter:card" content="jonnie_grieve_photography">
+        <!-- TWITTER: Open Graph -->
+        <meta name="twitter:title" content="<?php echo $page_title; ?> ">
+        <meta name="twitter:description" content="<?php echo $page_description; ?>">
+        <meta name="twitter:image" content="<?php echo $page_image; ?>">
+        <meta name="twitter:card" content="jonnie_grieve_photography">
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Merriweather|Odibee+Sans|Quicksand&display=swap" rel="stylesheet">
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather|Odibee+Sans|Quicksand&display=swap" rel="stylesheet">
 
-    <!-- Canonical Link -->
-    <link rel="canonical" href="<?php echo $page_url; ?>">
+        <!-- Canonical Link -->
+        <link rel="canonical" href="<?php echo $page_url; ?>">
 
-    <!-- Favicon -->  
-    <link rel="icon" href="" />
+        <!-- Favicon -->  
+        <link rel="icon" href="" />
 
-    <title> <?php echo $website_title ?></title>
+        <title> <?php echo $website_title ?></title>
 
-    <!-- Main Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="style.css">
+        <!-- Main Stylesheet -->
+        <link rel="stylesheet" type="text/css" href="style.css">
 
-</head>
-<body>
+    </head>
+    <body class="body---login">
 
-    <header class="header---login--page">
+        <header class="header---login--page">
 
-        <h1> <?php echo $website_title ?> </h1>
+            <h1> <?php echo $website_title ?> </h1>
 
-        <h2>Keep track of your way to your professional life</h2>
+            <h2>Keep track of your way to your professional life</h2>
 
-    </header>
+        </header>
 
-    <section class="container---element--login">
+        <section class="container---element--login">
 
-        <?php require "assets/template-parts/login.php"; ?>
+            <?php require "assets/template-parts/login.php"; ?>
 
-    </section>
+        </section>
 
-    <footer>
+        <footer>
 
-        <p>Copyright &copy; 2025 Jonathan Grieve</p>
+            <p>Copyright &copy; 2025 Jonathan Grieve</p>
 
-    </footer>
-    
+        </footer>
+        
 
-    <script type="text/javascript" src="app.js"></script>
-</body>
+        <script type="text/javascript" src="app.js"></script>
+    </body>
 </html>
