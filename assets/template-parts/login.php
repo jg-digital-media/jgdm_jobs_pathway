960775126b22c456d75101e20e27b72acc73ee45
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-15 10:00:25 +0100

    template parts for login and register forms

---
+++ assets/template-parts/login.php
@@ -0,0 +1,18 @@
+ <!-- Form: login form -->
+ <form name="main_form_login" action="list.php" method="post" id="" class="form form---login">
+
+    <label for="username">Username: </label>
+    <input type="text" name="username" class="login---input" id="username" placeholder="Enter your name">
+
+    <br>
+    <label for="email">Email: </label>
+    <input type="email" name="email" class="login---input" id="email" placeholder="Enter your email">
+
+    <br>
+    <label for="message">Password: </label>
+    <textarea name="message" id="message" class="login---input" cols="30" rows="10" placeholder="Enter your password"></textarea>
+
+    <br>
+    <input type="submit" value="Submit" class="button button---login">
+
+</form>
---
+++ assets/template-parts/register.php
@@ -0,0 +1,6 @@
+ <!-- Form: login form -->
+ <form name="main_form_register" action="list.php" method="post" id="" class="form form---register">
+   
+    <p>Registration Form</p>
+
+</form>
