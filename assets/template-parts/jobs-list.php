0e281f5cd0947987974ed9f59716ece749836dce
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-16 09:09:21 +0100

    add logo to top navigation banner

--- assets/template-parts/jobs-list.php
+++ assets/template-parts/jobs-list.php
@@ -1,1 +1,21 @@
 <p>jobs-list.php</p>
+
+<nav class="top---banner--container">
+
+    <img src="https://jgdm.s3.eu-west-2.amazonaws.com/logo.png" alt="Logo - <?php echo $website_title ?>" title="Logo - <?php echo $website_title ?>" id="jobs---pathway--logo"></li>
+
+    <ul class="app---navigation">
+        <li>
+            <a href="#">Progress Stats</a></li> 
+        <li>
+            <a href="#">How to Use!</a>
+        </li>
+    </ul>
+
+    <span id="motivation---message--container"> 
+
+        <p id="motivation---message">Keep up the good work! You'll get there!</p>
+
+    </span>
+
+</nav>
