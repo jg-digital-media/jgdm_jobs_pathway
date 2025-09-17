754d0a03d2b5a95b099b27d68e1248eea224afa8
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-17 07:42:22 +0100

    add new jobs profile template part

---
+++ assets/template-parts/job-profile.php
@@ -0,0 +1,46 @@
+
+<!-- Progress Stats Modal Goes Here -->
+
+
+<table class = "job---profile---container">
+      
+    <thead>
+        <tr>
+            <th colspan="2">Job Details</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Name of Company/Organisation:</td>
+            <td>Company name here!</td>
+        </tr>
+        <tr>
+            <td>Job Title:</td>
+            <td>Value goes here!</td>
+        </tr>
+        <tr>
+            <td>Salary Details:</td>
+            <td>Value goes here!</td>
+        </tr>
+        <tr>
+            <td>Job Location:</td>
+            <td>Value goes here!</td>
+        </tr>
+        <tr>
+            <td>Contact Person:</td>
+            <td>Value goes here!</td>
+        </tr>
+        <tr>
+            <td>Contact Person Email:</td>
+            <td><a href="mailto:contact_person@email.com">contact_person@email.com</a></td>
+        </tr>
+
+        <tr>
+            <td colspan="2" id="job---personal--notes">
+                <h4>Description:</h4>
+                
+                <p>Details go here....</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
