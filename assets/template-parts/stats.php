23d08377ffe836684a3ca0c9e98aa9c2b601be66
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-17 08:18:08 +0100

    build user stats dashboard

--- assets/template-parts/stats.php
+++ assets/template-parts/stats.php
@@ -5,7 +5,7 @@
 
 <a href="list.php">back</a>
 
-<table>
+<table class="job---profile--container">
     <thead> 
         <tr>
             <th>Active Jobs</th>
@@ -15,30 +15,32 @@
     <tbody>
         <tr>
             <td>Applications Sent:</td>
-            <td><span>1</span>/<span class="jobs---applied" id="js_applications---count">6</span> (<span class="progress---precentage">16.7%</span>)</td>
+            <td><span>1</span>/<span class="jobs---applied" id="js_applications---count">6</span> (<span class="progress---percentage">16.7%</span>)</td>
         </tr>
         <tr>
             <td>CV/Resume Updated</td>
-            <td><span>1</span>/<span class="jobs---applied" id="js_cv---count">6</span> (<span class="progress---precentage">16.7%</span>)</td>
+            <td><span>1</span>/<span class="jobs---applied" id="js_cv---count">6</span> (<span class="progress---percentage">16.7%</span>)</td>
         </tr>
         <tr>
             <td>Interview Secured</td>
-            <td><span>1</span>/<span class="jobs---applied" id="js_interview---count">6</span> (<span class="progress---precentage">16.7%</span>)</td>
+            <td><span>1</span>/<span class="jobs---applied" id="js_interview---count">6</span> (<span class="progress---percentage">16.7%</span>)</td>
         </tr>
         <tr>
             <td>Interview Attended</td>
-            <td><span>1</span>/<span class="jobs---applied" id="js_interview---attended--count">6</span> (<span class="progress---precentage">16.7%</span>)</td>
+            <td><span>1</span>/<span class="jobs---applied" id="js_interview---attended--count">6</span> (<span class="progress---percentage">16.7%</span>)</td>
         </tr>
         <tr>
             <td>References</td>
-            <td><span>1</span>/<span class="jobs---applied" id="js_references--count">6</span> (<span class="progress---precentage">16.7%</span>)</td>
+            <td><span>1</span>/<span class="jobs---applied" id="js_references--count">6</span> (<span class="progress---percentage">16.7%</span>)</td>
         </tr>
 
     </tbody>
-    <tfoot>       
+    <tfoot id="in--work--msg">       
         <tr>
             <td colspan="2">
-                 You're in work! Congratulations! <span id="in--work--tick"></span>
+
+                <p>You're in work! Congratulations! <span id="in--work--tick"></span></p>
+
             </td>
         </tr>  
     </tfoot>
