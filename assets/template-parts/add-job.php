60ec11e0fb2be763ad503c2d0002be28527ae668
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-17 08:55:07 +0100

    add job details form

--- assets/template-parts/add-job.php
+++ assets/template-parts/add-job.php
@@ -1,31 +1,39 @@
  <!-- Form: add job -->
- <form name="main_form_login" action="list.php" method="post" id="" class="form form---login">
 
-    <label for="company_name">Name of Company/Organisation: </label>
-    <input type="text" name="company_name" class="login---input" id="company_name" placeholder="Enter your name...">
+<section class="section---add--situation-container">
 
-    <br>
-    <label for="job_title">Job Title: </label>
-    <input type="text" name="job_title" class="login---input" id="job_title" placeholder="Job Title...">
+    <h3>Add a Situation/Job</h3>
 
-    <br>
-    <label for="salary">Salary: </label>
-    <input type="text" name="salary" class="login---input" id="salary" placeholder="Enter Salary...">
+    <form name="main_form_login" action="list.php" method="post" id="" class="form form---login">
 
-    <br>
-    <label for="location">Location: </label>
-    <input type="text" name="location" class="login---input" id="location" placeholder="Enter Salary...">
+        <label for="company_name">Name of Company/Organisation: </label>
+        <input type="text" name="company_name" class="login---input" id="company_name" placeholder="Enter your name...">
 
-    <br>
-    <label for="contact_person">Contact Person: </label>
-    <input type="contact_person"  name="contact_person" id="contact_person" class="login---input" placeholder="ontact P..">
+        <br>
+        <label for="job_title">Job Title: </label>
+        <input type="text" name="job_title" class="login---input" id="job_title" placeholder="Job Title...">
 
-    <br><br>
+        <br>
+        <label for="salary">Salary: </label>
+        <input type="text" name="salary" class="login---input" id="salary" placeholder="Enter Salary...">
 
-    <label for="description">Description: </label>
-    <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter your description..."></textarea>
+        <br>
+        <label for="location">Location: </label>
+        <input type="text" name="location" class="login---input" id="location" placeholder="Enter Salary...">
+
+        <br>
+        <label for="contact_person">Contact Person: </label>
+        <input type="contact_person"  name="contact_person" id="contact_person" class="login---input" placeholder="contact P..">
+
+        <br><br>
+
+        <label for="description">Description: </label>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter your description..."></textarea>
 
 
-    <input type="submit" value="Submit" class="button button---login">
+        <br /><br />
+        <input type="submit" value="Submit" class="button button---login">
 
-</form>
+    </form>
+
+</section>
