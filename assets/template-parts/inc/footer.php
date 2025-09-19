7e12c117f86030f47234e6da737e01741757f596
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-09-19 14:49:32 +0100

    app v0.0.7

--- assets/template-parts/inc/footer.php
+++ assets/template-parts/inc/footer.php
@@ -2,7 +2,7 @@
 
         <footer>
 
-            <p>Copyright &copy; 2025 Jonathan Grieve (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v0.0.6</a></span>)</p>
+            <p>Copyright &copy; 2025 Jonathan Grieve (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v0.0.7</a></span>)</p>
 
         </footer>        
 
