43d5accbbd53f254b4835f948c21836f62a620f9
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-10-27 09:33:48 +0000

    add custom post type by code

--- wp-content/themes/jobs_pathway_theme/functions.php
+++ wp-content/themes/jobs_pathway_theme/functions.php
@@ -1,3 +1,48 @@
 <?php 
 
-//echo "functions.php";
+// echo "functions.php";
+
+// Add custom post type
+//function jt_register_job_application_cpt() {
+
+    /* register_post_type('job_application', [
+
+        $labels = array(
+
+            'name'                => _x( 'Job Applications', 'Post Type General Name', 'jobs_pathway_theme' ),
+            'singular_name'       => _x( 'Job Application', 'Post Type Singular Name', 'jobs_pathway_theme' ),
+            'public'              => true,
+            'menu_name'           => __( 'Jobs', 'jobs_pathway_theme' ),
+            'name_admin_bar'      => __( 'Job', 'jobs_pathway_theme' ),
+            'archives'            => __( 'Job Archives', 'jobs_pathway_theme' ),
+            'has_archive'         => true,
+            'menu_icon' => 'dashicons-portfolio',
+            'supports' => ['title', 'editor', 'author'],
+            'show_in_rest' => true, // allows Gutenberg / REST API
+        )
+    ]);
+}
+
+add_action('init', 'jt_register_job_application_cpt'); */
+
+// add_action('init', $labels);
+
+// Add custom post type
+function jt_register_job_application_cpt() {
+
+    register_post_type('job_application', [
+
+        'labels' => [
+            'name' => 'Job Applications',
+            'singular_name' => 'Job Application'
+        ],
+        
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-portfolio',
+        'supports' => ['title', 'editor', 'author'],
+        'show_in_rest' => true, // allows Gutenberg / REST API
+    ]);
+}
+
+add_action('init', 'jt_register_job_application_cpt');
