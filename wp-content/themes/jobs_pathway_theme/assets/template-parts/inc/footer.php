c784c519a629ee8b6bf0d9a62bfaf5425b6048cc
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-11-03 15:56:16 +0000

    modify footer and copyright text

--- wp-content/themes/jobs_pathway_theme/assets/template-parts/inc/footer.php
+++ wp-content/themes/jobs_pathway_theme/assets/template-parts/inc/footer.php
@@ -1,8 +1,7 @@
-
-
+ 
         <footer>
 
-            <p>Copyright &copy; 2025 Jonathan Grieve (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v0.0.7</a></span>)</p>
+            <p>Designed by <a href="http://www.jonniegrieve.co.uk" target="_blank" >Jonnie Grieve Digital Media</a> Lovingly Built with WordPress. Developed with AI. Copyright &copy; 2025 Jonathan Grieve (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v2.0.6</a></span>)</p>
 
         </footer>        
 
