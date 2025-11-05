dfa1c38a7253d00d2e843e7232fed056ca5ef71e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-11-05 12:34:55 +0000

    start planning page copy

--- wp-content/themes/jobs_pathway_theme/page-howto.php
+++ wp-content/themes/jobs_pathway_theme/page-howto.php
@@ -29,7 +29,42 @@ $website_description = "Keep track of your way to your professional life";
 
         <section class="section---howto">
 
-            <p>Watch Back for more details soon</p>
+            <!-- <p>Watch Back for more details soon</p> -->
+
+            <article class="article---howto">
+
+                <h4 class="heading---howto">How to use the app</h4>
+
+                <div id="content---howto">
+
+                    <p>With "Jobs Pathway", you can easily organise the jobs you're applying for and tickoff each stage of the process as you you go.</p>
+
+                    <ul>
+                        <li>Step 1 - Look at your dashboard</li>
+                        <li>Step 2</li>
+                    </ul>
+
+                    <dl>
+                        <dt><strong>Step 1</strong>  - Look at your dashboard</dt>
+                        <dd>Step 1 details</dd>
+                        <dt><strong>Step 2</strong>  - Add a new job</dt>
+                        <dd>Step 2 details</dd>
+                        <dt><strong>Step 3</strong>  - Tick off your progress for each job in real time. </dt>
+                        <dd>Step 3 details</dd>
+                        <dt><strong>Step 4</strong>  - Add notes and details about each job you have applied for. </dt>
+                        <dd>Step 4 details</dd>
+                        <dt><strong>Step 5</strong>  - Progress Stats</dt>
+                        <dd>Step 5 details</dd> 
+                        <dt><strong>Step 6</strong>  - Delete Jobs</dt>
+                        <dd>Step 6 details</dd>
+                    </dl>
+
+                    <!-- <p>The app will help you keep track of the stages of your application process. You can tick off each stage as you go along. The app will also help you keep track of the jobs you have applied for and the progress you have made in each of them.</p>
+
+                    <p>You can also add notes and details about each job you have applied for. This will help you keep track of the jobs you have applied for and the progress you have made in each of them.</p> -->
+
+
+                </div>
 
         </section>
 
