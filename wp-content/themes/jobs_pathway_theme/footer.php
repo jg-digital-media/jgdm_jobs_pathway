ec8ea358155b6a3ac80003ac5b55638b695f0fef
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-11-10 07:23:07 +0000

    update app version

--- wp-content/themes/jobs_pathway_theme/footer.php
+++ wp-content/themes/jobs_pathway_theme/footer.php
@@ -3,7 +3,7 @@
 
         <footer>
 
-            <p>Designed by <a href="http://www.jonniegrieve.co.uk" target="_blank" >Jonnie Grieve Digital Media</a>. Lovingly built with WordPress. Developed with AI. Copyright &copy; 2025 All Rights Reserved (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v2.0.6</a></span>)</p>
+            <p>Designed by <a href="http://www.jonniegrieve.co.uk" target="_blank" >Jonnie Grieve Digital Media</a>. Lovingly built with WordPress. Developed with AI. Copyright &copy; 2025 All Rights Reserved (<span><a href="https://github.com/jg-digital-media/jgdm_jobs_pathway?tab=readme-ov-file#development" target="_blank">v2.0.11</a></span>)</p>
 
         </footer>   
 
